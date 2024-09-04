655204b0109eafcb09e53c52b3236e6671b402c5
albertfreeman <albert.freeman@gmail.com>
2024-09-04 13:37:51 -0500

    removed ui from ims

--- ims.py
+++ ims.py
@@ -9,125 +9,17 @@ import argparse
 import logging
 import json
 import os
-import pyglet
 
-from collections import namedtuple
-from enum import Enum
 from pathlib import Path
-from pyglet.font import ttf
 from rich.logging import RichHandler
-from rich.pretty import pprint
 
 logging.basicConfig(level='NOTSET', format='%(message)s', datefmt='[%X]', handlers=[RichHandler(rich_tracebacks=True)])
 logger = logging.getLogger(__name__)
 
 logging.info(f'{VERSION_INFO} started')
 
-Point = namedtuple('Point', ['x', 'y'])
-FrameSize = namedtuple('FrameSize', ['width', 'height'])
-
-screen = pyglet.canvas.get_display().get_default_screen()
-# window_size = FrameSize(width=(screen.width // 3) * 2, height=screen.height)
-window_size = FrameSize(1280, 720)
-logging.debug(f'Window Size: {window_size}')
-
-DEFAULT_INNER_MARGIN = 8
-MINIMUM_RESOLUTION = FrameSize(920, 640)
 DEFAULT_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tif', '.tiff', '.webp', '.svg']
 MAX_ALBUMS = 36  # no mod, shift, alt, ctrl * 9 (on the numeric keypad)
-TITLE_FONT_FILE = (Path('./resources/luckiest-guy/LuckiestGuy.ttf')
-                   #.expanduser()
-                   #.as_posix())
-
-
-class FontSizes(Enum):
-    TITLE = 64
-    SUBTITLE = 32
-    NORMAL = 24
-    SMALL = 18
-    TINY = 12
-
-
-class Colors(Enum):
-    TITLE = (200, 200, 0, 255)
-    SUBTITLE = (200, 200, 200, 255)
-    NORMAL = (200, 200, 200, 255)
-    SMALL = (200, 200, 200, 255)
-    TINY = (200, 200, 200, 255)
-    FRAME = (255, 255, 255, 64)
-
-
-class Stage:
-    def __init__(self, frame_size: FrameSize, max_horizontal: int, max_vertical: int,
-                 margin: int = DEFAULT_INNER_MARGIN) -> None:
-        self._max_horizontal = max_horizontal
-        self._max_vertical = max_vertical
-        self._frame_size = int(frame_size.width * (self._max_horizontal / 100))
-        self._window_height = int(frame_size.height * (self._max_vertical / 100))
-        self._sizing = Sizing(self._window_width, self._window_height, margin)
-
-
-class Sizing:
-    def __init__(self, width: int, height: int, margin: int = DEFAULT_INNER_MARGIN) -> None:
-        if width < MINIMUM_RESOLUTION.width:
-            width = MINIMUM_RESOLUTION.width
-            logging.debug(f'Minimum Window Width: {width}')
-
-        if height < MINIMUM_RESOLUTION.height:
-            height = MINIMUM_RESOLUTION.height
-            logging.debug(f'Minimum Window Height: {height}')
-
-        if margin < 0:
-            margin = 0
-
-        self._width = width
-        self._height = height
-        self._margin = margin
-        self._anchors = {
-            'left': {
-                'top': Point(0 + self._margin, self._height - self._margin),
-                'center': Point(0 + self._margin, self._height // 2),
-                'bottom': Point(0 + self._margin, 0 + self._margin)
-            },
-            'right': {
-                'top': Point(self._width - self._margin, self._height - self._margin),
-                'center': Point(self._width - self._margin, self._height // 2),
-                'bottom': Point(self._width - self._margin, 0 + self._margin)
-            },
-            'top': {
-                'left': Point(0 + self._margin, self._height - self._margin),
-                'center': Point(self._width // 2, self._height - self._margin),
-                'right': Point(self._width - self._margin, self._height - self._margin)
-            },
-            'bottom': {
-                'left': Point(0 + self._margin, 0 + self._margin),
-                'center': Point(self._width // 2, 0 + self._margin),
-                'right': Point(self._width - self._margin, 0 + self._margin)
-            },
-            'center': {
-                'top': Point(self._width // 2, self._height - self._margin),
-                'center': Point(self._width // 2, self._height // 2),
-                'bottom': Point(self._width // 2, 0 + self._margin),
-                'left': Point(0 + self._margin, self._height // 2),
-                'right': Point(self._width - self._margin, self._height // 2)
-            }
-        }
-
-    def get_position(self, anchor_x: pyglet.customtypes.HorizontalAlign,
-                     anchor_y: pyglet.customtypes.ContentVAlign) -> tuple:
-        return Point(self._anchors[anchor_x][anchor_y].x, self._anchors[anchor_x][anchor_y].y)
-
-    def dump(self) -> None:
-        if logger.level <= logging.DEBUG:
-            pprint('=' * 40)
-            pprint(f'Window Size: {self._width}x{self._height}')
-            pprint(f'Margin: {self._margin}')
-            pprint(f'Anchors: {self._anchors}')
-            pprint('=' * 40)
-
-
-anchors = Sizing(window_size.width, window_size.height)
-anchors.dump()
 
 
 def is_album(p: Path) -> bool:
@@ -140,11 +32,7 @@ def is_not_dotted(p: Path) -> bool:
 
 def move_item(source_file: Path, destination_folder: Path) -> None:
     logger.info(f'Moving {source_file} to {destination_folder}')
-
-
-def get_font(font_file: Path) -> str:
-    pyglet.font.add_file(TITLE_FONT_FILE)
-    return ttf.TruetypeInfo(TITLE_FONT_FILE).get_name('name')
+    source_file.rename(destination_folder / source_file.name)
 
 
 # ## Main #############################
@@ -193,60 +81,9 @@ if len(album_list) < MAX_ALBUMS:
     logging.info(f'Album directory {album_directory} has {len(album_list)} albums - padding to {MAX_ALBUMS}')
     album_list = album_list + ['-'] * (MAX_ALBUMS - len(album_list))
 if len(album_list) > MAX_ALBUMS:
-    logging.critical(f'Album directory {album_directory} has too many albums - truncating to {MAX_ALBUMS}')
+    logging.warning(f'Album directory {album_directory} has too many albums - truncating to {MAX_ALBUMS}')
     album_list = album_list[:MAX_ALBUMS]
 
 item_list = [Path(f).expanduser() for f in source_directory.iterdir() if
              Path(f).expanduser().suffix in DEFAULT_EXTENSIONS]
 logger.info(f'{len(item_list)} items found in {source_directory}')
-
-title_font = get_font(TITLE_FONT_FILE)
-main_window = pyglet.window.Window(resizable=False, width=window_size.width, height=window_size.height,
-                                   caption=VERSION_INFO)
-title = pyglet.text.Label(PROGRAM_NAME,
-                          font_name=title_font,
-                          font_size=FontSizes.TITLE.value,
-                          color=Colors.TITLE.value,
-                          x=anchors.get_position('left', 'top').x,
-                          y=anchors.get_position('left', 'top').y,
-                          anchor_x='left', anchor_y='top')
-copyright = pyglet.text.Label('Copyright 2024, Albert Freeman, MIT License',
-                              font_size=FontSizes.TINY.value,
-                              color=Colors.TINY.value,
-                              x=anchors.get_position('left', 'bottom').x,
-                              y=anchors.get_position('left', 'bottom').y,
-                              anchor_x='left', anchor_y='bottom')
-
-
-@main_window.event
-def on_draw() -> None:
-    main_window.clear()
-    title.draw()
-    copyright.draw()
-
-
-@main_window.event
-def on_resize(width: int, height: int) -> None:
-    # if we're too small, just resize to the minimum
-    if width < MINIMUM_RESOLUTION.width:
-        main_window.width = MINIMUM_RESOLUTION.width
-        width = MINIMUM_RESOLUTION.width
-        logging.debug(f'Minimum Window Width: {main_window.width}')
-
-    if height < MINIMUM_RESOLUTION.height:
-        main_window.height = MINIMUM_RESOLUTION.height
-        height = MINIMUM_RESOLUTION.height
-        logging.debug(f'MinimumWindow Height: {main_window.height}')
-
-    updated_window_size = FrameSize(width, height)
-    logger.debug(f'Window Size: {updated_window_size}')
-
-    updated_anchors = Sizing(updated_window_size.width, updated_window_size.height)
-    logger.debug(f'Window Anchors: {updated_anchors}')
-
-    # there's probably a better way of handling this
-    title.x, title.y = updated_anchors.get_position('left', 'top')
-    copyright.x, copyright.y = updated_anchors.get_position('left', 'bottom')
-
-
-pyglet.app.run()
--- res.py
+++ res.py
@@ -12,89 +12,108 @@ logger = logging.getLogger(__name__)
 logger.info('res starting')
 import pyglet
 
+Point = namedtuple('Point', ['x', 'y'])
+Color = namedtuple('Color', ['r', 'g', 'b'])
+
 RES_HORIZONTAL = 1280
 RES_VERTICAL = 720
-BOX_WIDTH_HORIZONTAL = 16
-BOX_HEIGHT_VERTICAL = 16
-MAX_LINES = RES_VERTICAL // BOX_HEIGHT_VERTICAL
-MAX_COLUMNS = RES_HORIZONTAL // BOX_WIDTH_HORIZONTAL
+BOX_WIDTH = 16
+BOX_HEIGHT = 16
+MAX_LINES = RES_VERTICAL // BOX_HEIGHT
+MAX_COLUMNS = RES_HORIZONTAL // BOX_WIDTH
 GRID_COLOR = (255, 255, 255, 48)
 LINE_WIDTH = 1.5
+TITLE_FONT = 'resources/api.ttf'
+
+
+def shade(self, color: str, alpha: float) -> Color:
+    _colors = {
+        'black': Color(0, 0, 0),
+        'white': Color(255, 255, 255),
+        'red': Color(255, 0, 0),
+        'green': Color(0, 255, 0),
+        'blue': Color(0, 0, 255),
+        'yellow': Color(255, 255, 0),
+        'cyan': Color(0, 255, 255),
+        'magenta': Color(255, 0, 255),
+    }
+    amount = int(255 * alpha)
+    r, g, b = _colors[color.lower()]
+    return Color(r, g, b, amount)
 
 
 class FontSizes(Enum):
-    TITLE = 64
+    TITLE = BOX_HEIGHT * 2
     SUBTITLE = 32
     NORMAL = 16
     SMALL = 10
     TINY = 8
 
 
-Point = namedtuple('Point', ['x', 'y'])
-
-line_column = (
-    lambda line, col: Point((col - 1) * BOX_WIDTH_HORIZONTAL, RES_VERTICAL - ((line - 1) * BOX_HEIGHT_VERTICAL)))
+line_column = (lambda line, col: Point((col - 1) * BOX_WIDTH, RES_VERTICAL - ((line - 1) * BOX_HEIGHT)))
 
 
 def get_line_column(line: int, column: int) -> Point:
-    x = (column - 1) * BOX_WIDTH_HORIZONTAL
-    y = RES_VERTICAL - ((line - 1) * BOX_HEIGHT_VERTICAL)
+    x = (column - 1) * BOX_WIDTH
+    y = RES_VERTICAL - ((line - 1) * BOX_HEIGHT)
     return Point(x, y)
 
 
 logger.debug(f'Max Lines: {MAX_LINES} Max Columns: {MAX_COLUMNS}')
 
+grid_batch = pyglet.graphics.Batch()
+horizontal_lines = pyglet.graphics.Group(order=0)
+vertical_lines = pyglet.graphics.Group(order=1)
+h_lines = []
+logger.info(f'horizontal grid lines')
+for h_line_y in range(1, RES_VERTICAL, BOX_HEIGHT):
+    p1 = Point(0, h_line_y)
+    p2 = Point(RES_HORIZONTAL, h_line_y)
+    logger.info(f'x1:{p1.x:04.2f} y1:{p1.y:04.2f} x2:{p2.x:04.2f} y2:{p2.y:04.2f}')
+    h_lines.append(pyglet.shapes.Line(x=p1.x,
+                                      y=p1.y,
+                                      x2=p2.x,
+                                      y2=p2.y,
+                                      width=LINE_WIDTH,
+                                      color=GRID_COLOR,
+                                      batch=grid_batch,
+                                      group=horizontal_lines,
+                                      blend_src=pyglet.gl.GL_SRC_ALPHA,
+                                      blend_dest=pyglet.gl.GL_ONE_MINUS_DST_ALPHA))
 
-def mk_grid(horizontal_resolution: int, box_width: int,
-            vertical_resolution: int, box_height: int,
-            grid_color: (int, int, int, int),
-            line_width: float) -> pyglet.graphics.Batch:
-    batch = pyglet.graphics.Batch()
-    h_lines = []
-    logger.info(f'horizontal grid lines')
-    for h_line_y in range(1, vertical_resolution, box_height):
-        p1 = Point(0, h_line_y)
-        p2 = Point(horizontal_resolution, h_line_y)
-        logger.info(f'x1:{p1.x:04.2f} y1:{p1.y:04.2f} x2:{p2.x:04.2f} y2:{p2.y:04.2f}')
-        h_lines.append(pyglet.shapes.Line(x=p1.x,
-                                          y=p1.y,
-                                          x2=p2.x,
-                                          y2=p2.y,
-                                          width=line_width,
-                                          color=grid_color,
-                                          batch=batch,
-                                          blend_src=pyglet.gl.GL_SRC_ALPHA,
-                                          blend_dest=pyglet.gl.GL_ONE_MINUS_DST_ALPHA))
+v_lines = []
+logger.info(f'vertical grid lines')
+for y_line_x in range(1, RES_HORIZONTAL, BOX_WIDTH):
+    p1 = Point(y_line_x, 0)
+    p2 = Point(y_line_x, RES_VERTICAL)
+    logger.info(f'x1:{p1.x:04.2f} y1:{p1.y:04.2f} x2:{p2.x:04.2f} y2:{p2.y:04.2f}')
+    v_lines.append(pyglet.shapes.Line(x=p1.x,
+                                      y=p1.y,
+                                      x2=p2.x,
+                                      y2=p2.y,
+                                      width=LINE_WIDTH,
+                                      color=GRID_COLOR,
+                                      batch=grid_batch,
+                                      group=vertical_lines,
+                                      blend_src=pyglet.gl.GL_SRC_ALPHA,
+                                      blend_dest=pyglet.gl.GL_ONE_MINUS_SRC_ALPHA))
 
-    v_lines = []
-    logger.info(f'vertical grid lines')
-    for y_line_x in range(1, horizontal_resolution, box_width):
-        p1 = Point(y_line_x, 0)
-        p2 = Point(y_line_x, vertical_resolution)
-        logger.info(f'x1:{p1.x:04.2f} y1:{p1.y:04.2f} x2:{p2.x:04.2f} y2:{p2.y:04.2f}')
-        v_lines.append(pyglet.shapes.Line(x=p1.x,
-                                          y=p1.y,
-                                          x2=p2.x,
-                                          y2=p2.y,
-                                          width=line_width,
-                                          color=grid_color,
-                                          batch=batch,
-                                          blend_src=pyglet.gl.GL_SRC_ALPHA,
-                                          blend_dest=pyglet.gl.GL_ONE_MINUS_SRC_ALPHA))
-
-    pprint(batch.__dict__)
-    return batch
-
-
-grid_batch = mk_grid(RES_HORIZONTAL, BOX_WIDTH_HORIZONTAL, RES_VERTICAL, BOX_HEIGHT_VERTICAL, GRID_COLOR, LINE_WIDTH)
 p_title = line_column(2, 2)
+pyglet.font.add_file(TITLE_FONT)
+title_font = pyglet.font.load('Armor Piercing', 64)
 logger.info(f'x1:{p_title.x:04.2f} y1:{p_title.y:04.2f}')
-title_label = pyglet.text.Label('Resolution & Grid Test',
-                                font_size=FontSizes.TINY.value,
+
+title_label = pyglet.text.Label('IMGSACK',
+                                font_name='Armor Piercing',
+                                font_size=FontSizes.TITLE.value,
                                 bold=True,
                                 color=(200, 200, 255, 255),
                                 anchor_x='left', anchor_y='top',
                                 x=p_title.x, y=p_title.y)
+
+stage_point = get_line_column(2,2)
+logger.info(f'x1:{stage_point.x:04.2f} y1:{stage_point.y:04.2f}')
+shape_stage = pyglet.shapes.Box(stage_point.x, stage_point.y, width=640,height=640, color=(255, 255, 255, 160), anchor_x='left', anchor_y='top')
 
 main_window = pyglet.window.Window(resizable=False,
                                    width=1280,
@@ -107,6 +126,7 @@ def on_draw():
     main_window.clear()
     grid_batch.draw()
     title_label.draw()
+    shape_stage.draw()
 
 
 @main_window.event
