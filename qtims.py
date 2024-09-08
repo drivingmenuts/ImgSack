1882df15f50c5ae24a11abc01bccc794c912ae55
albertfreeman <albert.freeman@gmail.com>
2024-09-08 03:32:55 -0500

    Mostly working!

--- qtims.py
+++ qtims.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-PROGRAM_NAME = 'ImageSack'
-PROGRAM_VERSION = '0.0.1rNone'
-PYTHON_VERSION = '3.9'
-VERSION_INFO = f'{PROGRAM_NAME} v{PROGRAM_VERSION} - {PYTHON_VERSION}'
-
-import qtkeymapper
+PROGRAM_NAME = "ImageSack"
+PROGRAM_VERSION = "0.0.1rNone"
+PYTHON_VERSION = "3.9"
+VERSION_INFO = f"{PROGRAM_NAME} v{PROGRAM_VERSION} - {PYTHON_VERSION}"
 
 import argparse
 import logging
@@ -13,52 +11,92 @@ import json
 import os
 import sys
 
+from enum import Enum
 from pathlib import Path
 from rich.logging import RichHandler
 
-logging.basicConfig(level='NOTSET', format='%(message)s', datefmt='[%X]', handlers=[RichHandler(rich_tracebacks=True)])
-logger = logging.getLogger(__name__)
-
-logging.info(f'{VERSION_INFO} started')
-
-NO_ALBUM_MESSAGE = 'No albums defined'
-DEFAULT_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tif', '.tiff', '.webp', '.svg']
-MAX_ALBUMS = 36  # no mod, shift, alt, ctrl * 9 (on the numeric keypad)
-
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
+from PySide6.QtGui import *
 
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
+logger = logging.getLogger(__name__)
+
+logging.info(f"{VERSION_INFO} started")
+
+NO_ALBUM_MESSAGE = "No album defined"
+DEFAULT_EXTENSIONS = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".tif",
+    ".tiff",
+    ".webp",
+    ".svg",
+]
+MAX_ALBUMS = 36  # no mod, shift, alt, ctrl * 9 (on the numeric keypad)
+
+
+class FontSize(Enum):
+    TITLE = 24
+    SUBTITLE = 20
+    NORMAL = 16
+    SMALL = 12
+    TINY = 10
+
+
+# QT swaps key names for modifiers on MacOS. This switches them back.
+MODIFIER_KEYS = ["", "Shift", "Ctrl", "Alt"]
+KEYPRESS_VALUES = [-1, 16777248, 16777250, 16777251]
 DEBUG = True
+RIGHT_COLUMN_WIDTH = 384
 
 
 class LabelSetWidget(QFrame):
-    def __init__(self, label: str, buttons=None, parent=None):
+    def __init__(self, title: str, buttons=None, parent=None):
         super().__init__(parent)
         if buttons is None:
             buttons = []
         layout = QVBoxLayout()
 
-        title_label = QLabel(label)
-        title_label.setMinimumWidth(384)
+        title_label = QLabel(title)
+        title_label.setMinimumWidth(RIGHT_COLUMN_WIDTH)
+        title_label.setMaximumWidth(RIGHT_COLUMN_WIDTH)
+        title_label.setStyleSheet(
+            f"font-size: {FontSize.SUBTITLE.value}px; font-weight: bold;"
+        )
         title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(title_label)
 
+        key_counter = 1
         for button_text in buttons:
-            button = QPushButton(button_text)
+            button = QPushButton(f"{key_counter}. {button_text}")
+            button.setStyleSheet(f"font-size: {FontSize.NORMAL.value}px;")
+            button.setShortcut(QKeySequence(f"{key_counter}"))
             layout.addWidget(button)
+            key_counter += 1
 
         if len(buttons) < 9:
             for _i in range(9 - len(buttons)):
                 button = QPushButton(NO_ALBUM_MESSAGE)
+                button.setDisabled(True)
+                # button = QPushButton(f"{key_counter}. Nope!")
+                button.setStyleSheet(f"font-size: {FontSize.NORMAL.value}px;")
+                # button.setShortcut(QKeySequence(f'{key_counter}'))
                 layout.addWidget(button)
-
-        if DEBUG:
-            logger.debug(r'Adding frame')
-            self.setFrameShape(self.Shape.Box)
-            # pass
 
         layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.setLayout(layout)
+
+
+from TrackedWindow import *
 
 
 class MainWindow(QMainWindow):
@@ -69,53 +107,63 @@ class MainWindow(QMainWindow):
 
         main_layout = QHBoxLayout()
 
-        image_label = QLabel("Image")
+        image_label = QLabel(
+            "ImgSack\nAlbert Freeman\nhttps://github.com/drivigmenuts/ImgSack"
+        )
         image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         image_label.setMinimumWidth(768)
         image_label.setMinimumHeight(768)
+        if DEBUG:
+            logger.debug(r"Adding frame")
+            image_label.setFrameShape(QFrame.Shape.Box)
         main_layout.addWidget(image_label)
 
-        no_modifier_widget = LabelSetWidget("None")
-        alt_modifier_widget = LabelSetWidget("Alt")
-        ctrl_modifier_widget = LabelSetWidget("Ctrl")
-        shift_modifier_widget = LabelSetWidget("Shift")
-
-        label_key_layout = QStackedLayout()
-        label_key_layout.addWidget(no_modifier_widget)
-        label_key_layout.addWidget(alt_modifier_widget)
-        label_key_layout.addWidget(ctrl_modifier_widget)
-        label_key_layout.addWidget(shift_modifier_widget)
+        self.label_key_layout = QStackedLayout()
+        for modifier in MODIFIER_KEYS:
+            labels = LabelSetWidget(modifier)
+            self.label_key_layout.addWidget(labels)
 
         utility_keys_layout = QHBoxLayout()
         skip_button_0 = QPushButton("0 - Skip")
+        skip_button_0.setStyleSheet(f"font-size: {FontSize.NORMAL.value}px;")
+        skip_button_0.setShortcut(QKeySequence("0"))
         trash_button_decimal = QPushButton(". - Trash")
+        trash_button_decimal.setStyleSheet(f"font-size: {FontSize.NORMAL.value}px;")
+        trash_button_decimal.setShortcut(QKeySequence("."))
 
         utility_keys_layout.addWidget(skip_button_0)
         utility_keys_layout.addWidget(trash_button_decimal)
 
         right_column_layout = QVBoxLayout()
-        right_column_layout.addLayout(label_key_layout)
+        right_column_layout.addLayout(self.label_key_layout)
         right_column_layout.addLayout(utility_keys_layout)
+        right_column_layout.addStretch()
 
-        main_layout.addLayout(right_column_layout)
+        right_column_container = QWidget()
+        right_column_container.setMinimumWidth(RIGHT_COLUMN_WIDTH)
+        right_column_container.setMaximumWidth(RIGHT_COLUMN_WIDTH)
+        right_column_container.setLayout(right_column_layout)
+
+        main_layout.addWidget(right_column_container)
+
+        # main_layout.addLayout(right_column_layout)
 
         central_widget = QWidget()
         central_widget.setLayout(main_layout)
 
         self.setCentralWidget(central_widget)
 
-        self.keyPressEventFilter = KeyPressEventFilter(self)
-        self.installEventFilter(self.keyPressEventFilter)
+    def keyPressEvent(self, event: QKeyEvent) -> QKeyEvent:
+        super().keyPressEvent(event)
+        if event.key() in KEYPRESS_VALUES:
+            self.label_key_layout.setCurrentIndex(KEYPRESS_VALUES.index(event.key()))
+        return event
 
-
-class KeyPressEventFilter(QObject):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-    def eventFilter(self, obj, event):
-        if event.type() == QEvent.KeyPress:
-            logger.info(f'key_press: {event.key()}')
-        return False
+    def keyReleaseEvent(self, event: QKeyEvent) -> QKeyEvent:
+        super().keyReleaseEvent(event)
+        if event.key() in KEYPRESS_VALUES:
+            self.label_key_layout.setCurrentIndex(0)
+        return event
 
 
 app = QApplication(sys.argv)
